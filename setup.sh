7f29a43fc2f7581d2fd11b38de9203f2b74b4788
Sanjay Kumaran <sakumaran@microsoft.com>
2024-03-14 01:13:03 -0700

    updated setup file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+chmod +x "$0"
+
+install_brew() {
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+
+# Function to install Zsh
+install_zsh() {
+    echo "Installing Zsh..."
+    if [ "$(uname)" == "Darwin" ]; then
+        # macOS
+        brew install zsh
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        # Linux
+        if command -v apt-get &> /dev/null; then
+            sudo apt-get update
+            sudo apt-get install zsh
+        elif command -v yum &> /dev/null; then
+            sudo yum install zsh
+        elif command -v pacman &> /dev/null; then
+            sudo pacman -Syu zsh
+        else
+            echo "Unable to install Zsh. Unsupported Linux distribution."
+            exit 1
+        fi
+    else
+        echo "Unable to install Zsh. Unsupported operating system."
+        exit 1
+    fi
+}
+
 # Check if Zsh is installed
 if ! command -v zsh &> /dev/null; then
-    echo "Zsh is not installed. Please install Zsh and re-run this script."
-    exit 1
+    echo "Zsh is not installed. Installing..."
+    install_zsh
 fi
 
 # Check if the default shell is already Zsh
@@ -25,4 +58,3 @@ exec zsh
 
 # Create symbolic link for .zshrc
 ln -sf ~/dotfile/.zshrc ~/.zshrc
-
